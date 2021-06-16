97cdfd31d5ac3d5b97e90d4aa095cc98cf26e625
Stanley <stanley.defer@hotmail.Fr>
2021-06-16 03:22:35 +0200

    🎉 Add rate limiter

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const path = require("path");
 const sauceRoutes = require("./routes/sauce");
 const userRoutes = require("./routes/user");
 
+const rateLimit = require("express-rate-limit");
+
 const app = express();
 
 require("dotenv").config();
@@ -33,7 +35,11 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
+
 app.use("/api/sauces", sauceRoutes);
+
 app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use(rateLimit());
 
 module.exports = app;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,19 @@
 const express = require("express");
+
 const router = express.Router();
+
 const userCtrl = require("../controllers/user");
 const emailValidation = require("../middleware/emailValidation");
 const passValidation = require("../middleware/passValidation");
+const rateLimit = require("express-rate-limit");
+
+const rateLimiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 10,
+    message: " Too many attempts, try again in 5 minutes",
+});
 
 router.post("/signup", emailValidation, passValidation, userCtrl.signup);
-router.post("/login", userCtrl.login);
+router.post("/login", rateLimiter, userCtrl.login);
 
 module.exports = router;
